f6c1c75ef1ef4aa7db168f2f6b73289d0b5e609c
sawyer27 <saawyeer@mail.ru>
2014-11-17 08:17:18 -0300

    update02

---
+++ source/config.php
@@ -0,0 +1,9 @@
+<?php
+
+// apikey developer riot
+define("api_key", "c85474da-c4a2-47da-9e81-403164f2d4ba");
+
+// servidores
+define("las", "las");
+
+?>
---
+++ source/index.php
@@ -0,0 +1,14 @@
+<html>
+
+ <head>
+  <title>Prueba de PHP</title>
+ </head>
+
+ <body>
+	 <?php 
+	 	require "config.php";
+
+	 	echo '<p>Hola Mundo</p>'; 
+	 ?>
+ </body>
+</html>
